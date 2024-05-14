24fda45e8fd595c3f2f073ff1618755536fb3933
taynaraluanacaetano <taynaraluanac@gmail.com>
2024-05-14 13:30:12 -0300

    ajustando_vercel

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,13 +1,13 @@
-const loginForm = document.getElementById('loginForm');
+const loginForm = document.getElementById("loginForm");
 
-loginForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    if (username === 'admin' && password === 'admin') {
-        window.location.href = 'menuPrincipal.html';
-    } else {
-        $('#loginErrorModal').modal('show');
-    }
+  if (username === "admin" && password === "admin") {
+    window.location.href = '/menuPrincipal.html'; // Redirecionamento para a página principal
+  } else {
+    $("#loginErrorModal").modal("show");
+  }
 });
