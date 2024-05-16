4f1f77d4e82a1a277746f8adaa9d54512ae4d6fc
taynaraluanacaetano <taynaraluanac@gmail.com>
2024-05-16 18:16:42 -0300

    implementacao

--- frontend/js/cadastroUsuarios.js
+++ frontend/js/cadastroUsuarios.js
@@ -11,20 +11,66 @@ document.getElementById("userForm").addEventListener("submit", function (event) 
     var telefone = document.getElementById("telefone").value;
     var email = document.getElementById("email").value;
     var senha = document.getElementById("senha").value;
+    var userId = document.getElementById("submitBtn").getAttribute("data-user-id");
 
-    axios.post("http://localhost:3000/users", {
-        nome: nome,
-        telefone: telefone,
-        email: email,
-        senha: senha
-    })
+    if (userId) {
+        axios.put(`http://localhost:3000/users/${userId}`, {
+            nome: nome,
+            telefone: telefone,
+            senha: senha
+        })
+            .then(function (response) {
+                clearForm();
+                console.log("Usuário atualizado com sucesso!");
+                $("#successModal").modal("show");
+                updateUserTable();
+
+                document.getElementById("submitBtn").innerText = "Cadastrar Usuário";
+                document.getElementById("userForm").reset();
+            })
+            .catch(function (error) {
+                console.error("Erro ao atualizar usuário:", error);
+            });
+    } else {
+        axios.post("http://localhost:3000/users", {
+            nome: nome,
+            telefone: telefone,
+            email: email,
+            senha: senha,
+        })
+            .then(function (response) {
+                clearForm();
+                console.log("Cadastro bem-sucedido!");
+                $("#successModal").modal("show");
+                updateUserTable();
+            })
+            .catch(function (error) {
+                console.error("Erro ao cadastrar usuário:", error);
+                if (
+                    error.response &&
+                    error.response.status === 400 &&
+                    error.response.data.error === "E-mail já está em uso"
+                ) {
+                    $("#emailInUseModal").modal("show");
+                }
+            });
+    }
+});
+
+$(document).on('click', '.btn-edit', function () {
+    var userId = $(this).data('user-id');
+    axios.get(`http://localhost:3000/users/${userId}`)
         .then(function (response) {
-            clearForm();
-            document.getElementById("successMessage").classList.remove("d-none");
-            updateUserTable();
+            document.getElementById("nome").value = response.data.nome;
+            document.getElementById("telefone").value = response.data.telefone;
+            document.getElementById("email").value = response.data.email;
+            document.getElementById("senha").value = response.data.senha;
+
+            document.getElementById("submitBtn").innerText = "Salvar Alterações";
+            document.getElementById("submitBtn").setAttribute("data-user-id", userId);
         })
         .catch(function (error) {
-            console.error("Erro ao cadastrar usuário:", error);
+            console.error("Erro ao obter detalhes do usuário para edição:", error);
         });
 });
 
@@ -37,30 +83,19 @@ function updateUserTable() {
             response.data.forEach(function (user) {
                 var newRow = userList.insertRow();
                 newRow.innerHTML = `
-                <td>${user.nome}</td>
-                <td>${user.telefone}</td>
-                <td>${user.email}</td>
-                <td>
-                    <button type="button" class="btn btn-sm btn-primary">Editar</button>
-                    <button type="button" class="btn btn-sm btn-danger">Excluir</button>
-                </td>
-            `;
+                    <td>${user.nome}</td>
+                    <td>${user.telefone}</td>
+                    <td>${user.email}</td>
+                    <td>
+                        <button type="button" class="btn btn-sm btn-primary btn-edit" data-user-id="${user.id}">Editar</button>
+                        <button type="button" class="btn btn-sm btn-danger">Excluir</button>
+                    </td>
+                `;
             });
         })
         .catch(function (error) {
             console.error("Erro ao obter usuários:", error);
         });
 }
-
-function formatarTelefone(telefone) {
-    var digits = telefone.replace(/\D/g, '');
-    return '(' + digits.substring(0, 2) + ') ' + digits.substring(2, 7) + '-' + digits.substring(7, 11);
-}
-
-document.getElementById('telefone').addEventListener('input', function (event) {
-    var telefone = event.target.value;
-
-    event.target.value = formatarTelefone(telefone);
-});
 
 updateUserTable();
