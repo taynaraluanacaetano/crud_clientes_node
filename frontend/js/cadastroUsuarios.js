19604a91f7246f19a14168d521a72bf51bfab035
taynaraluanacaetano <taynaraluanac@gmail.com>
2024-06-04 18:32:01 -0300

    teste_3

--- frontend/js/cadastroUsuarios.js
+++ frontend/js/cadastroUsuarios.js
@@ -5,59 +5,63 @@ function clearForm() {
   document.getElementById("senha").value = "";
 }
 
-document.getElementById("userForm").addEventListener("submit", function (event) {
-  event.preventDefault();
-  var nome = document.getElementById("nome").value;
-  var telefone = document.getElementById("telefone").value;
-  var email = document.getElementById("email").value;
-  var senha = document.getElementById("senha").value;
-  var userId = document.getElementById("submitBtn").getAttribute("data-user-id");
+document
+  .getElementById("userForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    var nome = document.getElementById("nome").value;
+    var telefone = document.getElementById("telefone").value;
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+    var userId = document
+      .getElementById("submitBtn")
+      .getAttribute("data-user-id");
 
-  if (userId) {
-    axios
-      .put(`http://localhost:3000/users/${userId}`, {
-        nome: nome,
-        telefone: telefone,
-        email: email,
-        senha: senha,
-      })
-      .then(function (response) {
-        clearForm();
-        console.log("Usuário atualizado com sucesso!");
-        $("#successModal").modal("show");
-        updateUserTable();
-        document.getElementById("submitBtn").innerText = "Cadastrar Usuário";
-        document.getElementById("userForm").reset();
-      })
-      .catch(function (error) {
-        console.error("Erro ao atualizar usuário:", error);
-      });
-  } else {
-    axios
-      .post("http://localhost:3000/users", {
-        nome: nome,
-        telefone: telefone,
-        email: email,
-        senha: senha,
-      })
-      .then(function (response) {
-        clearForm();
-        console.log("Cadastro bem-sucedido!");
-        $("#successModal").modal("show");
-        updateUserTable();
-      })
-      .catch(function (error) {
-        console.error("Erro ao cadastrar usuário:", error);
-        if (
-          error.response &&
-          error.response.status === 422 &&
-          error.response.data.error === "E-mail já está em uso"
-        ) {
-          $("#emailInUseModal").modal("show");
-        }
-      });
-  }
-});
+    if (userId) {
+      axios
+        .put(`http://localhost:3000/users/${userId}`, {
+          nome: nome,
+          telefone: telefone,
+          email: email,
+          senha: senha,
+        })
+        .then(function (response) {
+          clearForm();
+          console.log("Usuário atualizado com sucesso!");
+          $("#successModal").modal("show");
+          updateUserTable();
+          document.getElementById("submitBtn").innerText = "Cadastrar Usuário";
+          document.getElementById("userForm").reset();
+        })
+        .catch(function (error) {
+          console.error("Erro ao atualizar usuário:", error);
+        });
+    } else {
+      axios
+        .post("http://localhost:3000/users", {
+          nome: nome,
+          telefone: telefone,
+          email: email,
+          senha: senha,
+        })
+        .then(function (response) {
+          clearForm();
+          console.log("Cadastro bem-sucedido!");
+          $("#successModal").modal("show");
+          updateUserTable();
+        })
+        .catch(function (error) {
+          console.error("Erro ao cadastrar usuário:", error);
+          if (
+            error.response &&
+            error.response.status === 422 &&
+            error.response.data.error === "E-mail já está em uso"
+          ) {
+            $("#emailInUseModal").modal("show");
+          }
+        });
+    }
+  });
 
 $(document).on("click", ".btn-edit", function () {
   var userId = $(this).data("user-id");
@@ -100,7 +104,7 @@ function updateUserTable() {
       $(document).on("click", ".btn-delete", function () {
         var userId = $(this).data("user-id");
         $("#confirmDeleteModal").modal("show");
-        $(".btn-ok").data("user-id", userId); 
+        $(".btn-ok").data("user-id", userId);
       });
     })
     .catch(function (error) {
