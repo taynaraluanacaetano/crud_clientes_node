ff7211e0469278b4f8a09505b21256a00c1d8598
taynaraluanacaetano <taynaraluanac@gmail.com>
2024-06-22 08:07:39 -0300

    ajuste_doc

--- backend/routes/conteudoRoutes.js
+++ backend/routes/conteudoRoutes.js
@@ -3,13 +3,21 @@ const router = express.Router();
 const conteudoController = require('../controller/conteudoController');
 
 router.post('/', conteudoController.createConteudo);
-router.get('/', conteudoController.getAllConteudos)
+router.get('/', conteudoController.getAllConteudos);
 router.get('/:id', conteudoController.getConteudoById);
 router.delete('/:id', conteudoController.deleteConteudo);
 router.put('/:id', conteudoController.updateConteudo);
 
+/**
+ * @swagger
+ * tags:
+ *   name: Conteudos
+ *   description: API para gerenciamento de conteúdos
+ */
 
 /**
+ * 
+ * 
  * @swagger
  * components:
  *   schemas:
@@ -76,6 +84,74 @@ router.put('/:id', conteudoController.updateConteudo);
  *         description: Conteúdo criado com sucesso
  */
 
+/**
+ * @swagger
+ * /conteudos/{id}:
+ *   get:
+ *     summary: Retorna um conteúdo pelo ID
+ *     tags: [Conteudos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: O ID do conteúdo
+ *     responses:
+ *       200:
+ *         description: Conteúdo encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Conteudo'
+ *       404:
+ *         description: Conteúdo não encontrado
+ */
+
+/**
+ * @swagger
+ * /conteudos/{id}:
+ *   delete:
+ *     summary: Remove um conteúdo pelo ID
+ *     tags: [Conteudos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: O ID do conteúdo
+ *     responses:
+ *       200:
+ *         description: Conteúdo removido com sucesso
+ *       404:
+ *         description: Conteúdo não encontrado
+ */
+
+/**
+ * @swagger
+ * /conteudos/{id}:
+ *   put:
+ *     summary: Atualiza um conteúdo pelo ID
+ *     tags: [Conteudos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: O ID do conteúdo
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Conteudo'
+ *     responses:
+ *       201:
+ *         description: Conteúdo atualizado com sucesso
+ *       404:
+ *         description: Conteúdo não encontrado
+ */
 
 module.exports = router;
-
